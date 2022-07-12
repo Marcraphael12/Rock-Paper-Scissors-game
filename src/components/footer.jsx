0a4f058a93a5697580a6917a30f66fe375097850
Marc raphael <marcraphael979@gmail.com>
2022-07-12 10:50:34 +0100

    add button

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -1,8 +1,17 @@
+import styled from "styled-components";
+
 const Footer = () => {
-  
+ const Button = styled.button `
+  padding: 10px 20px;
+  border: solid 2px hsl(217, 16%, 45%);
+  color: hsl(229, 25%, 31%);
+  background: hsl(214, 47%, 23%);
+ `;
   return (
     <footer>
-
+      <Button>
+        Rules
+      </Button>
     </footer>
   );
 }

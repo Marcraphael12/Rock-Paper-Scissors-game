1ef2152fb96e2ba6f9fc58743d5f16b7b347eb85
Marc raphael <marcraphael979@gmail.com>
2022-07-12 10:57:29 +0100

    change text color

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -4,7 +4,7 @@ const Footer = () => {
  const Button = styled.button `
   padding: 10px 20px;
   border: solid 2px hsl(217, 16%, 45%);
-  color: hsl(229, 25%, 31%);
+  color: #fff;
   background: hsl(214, 47%, 23%);
  `;
   return (

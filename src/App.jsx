18adfde4e98a79f3dde4f8d078acbc2750dd96b6
Marc raphael <marcraphael979@gmail.com>
2022-07-12 10:57:55 +0100

    render home page

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,10 @@
 import React from 'react';
 import './App.scss';
+import Home from './components/game_page';
 
 function App() {
   return (
-    <div className="App">
-     Rock, Paper, scissor
-    </div>
+    <Home />
   );
 }
 

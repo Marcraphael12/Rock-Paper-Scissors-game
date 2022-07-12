072619f730e762ee5ac9f9b63a3b71b40bdcf9c9
Marc raphael <marcraphael979@gmail.com>
2022-07-12 10:20:36 +0100

    add header and footer components

---
+++ src/components/game_page.jsx
@@ -0,0 +1,13 @@
+import Header from './header';
+import Footer from './footer'
+
+const Home = ()=> {
+  return (
+    <>
+      <Header />
+      <Footer />
+    </>
+  )
+}
+
+export default Home;

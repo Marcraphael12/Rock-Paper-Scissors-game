21f9b7b5d6738a8595dc0c24cec8f2923b4ab172
Marc raphael <marcraphael979@gmail.com>
2022-07-16 08:17:00 +0100

    Write gamePlay function

---
+++ src/components/game/game_play.js
@@ -0,0 +1,10 @@
+// let's make our game
+// our computer choices
+import computer_choice from "./computer";
+
+const GamePlay = () => {
+	// we need the player choice
+	
+}
+ 
+export default GamePlay;

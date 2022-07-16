6fc30e31b0352e26ff3bf803c7bdd3a668136ede
Marc raphael <marcraphael979@gmail.com>
2022-07-16 08:17:49 +0100

    Import icons for buttons

--- src/components/game/game_play.js
+++ src/components/game/game_play.js
@@ -2,9 +2,16 @@
 // our computer choices
 import computer_choice from "./computer";
 
+// icons for buttons
+import icon_rock from '../../images/icon-rock.png';
+import icon_paper from '../../images/icon-paper.png';
+import icon_scissor from '../../images/icon-scissors.png';
+import icon_lizard from '../../images/icon-lizard.png';
+import icon_spock from '../../images/icon-spock.png';
+
 const GamePlay = () => {
 	// we need the player choice
-	
+
 }
  
 export default GamePlay;

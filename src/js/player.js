f3c9fac0b52cdeff0edf771fd4e13815b93cf47d
Marc raphael <marcraphael979@gmail.com>
2022-07-18 20:39:23 +0100

    Write events for player choice

--- src/js/player.js
+++ src/js/player.js
@@ -5,3 +5,33 @@ const lizard_btn = document.querySelector('.lizard');
 const spock_btn = document.querySelector('.spock');
 
 const all_buttons = [rock_btn, paper_btn, scissors_btn, lizard_btn, spock_btn];
+
+rock_btn.addEventListener('click', () => {
+	preventDefault();
+	console.log('rock');
+	return 'rock';
+})
+
+paper_btn.addEventListener('click', () => {
+	preventDefault();
+	console.log('paper');
+	return 'paper';
+})
+
+scissors_btn.addEventListener('click', () => {
+	preventDefault();
+	console.log('scissors');
+	return 'scissors';
+})
+
+lizard_btn.addEventListener('click', () => {
+	preventDefault();
+	console.log('lizard');
+	return 'lizard';
+})
+
+spock_btn.addEventListener('click', () => {
+	preventDefault();
+	console.log('spock');
+	return 'spock';
+})

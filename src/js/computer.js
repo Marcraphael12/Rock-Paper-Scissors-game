23cc144fde1d38e7971842b1bc68f3db0806b39a
Marc raphael <marcraphael979@gmail.com>
2022-07-18 17:15:08 +0100

    Add condition to get different choices

--- src/js/computer.js
+++ src/js/computer.js
@@ -1,6 +1,17 @@
 const ComputerChoice = () => {
 	const computerChoice = Math.floor(Math.random() * 4);
+	if (computerChoice === 0) {
+		return 'rock';
+	} else if (computerChoice === 1) {
+		return 'paper';
+	} else if (computerChoice === 2) {
+		return 'scissors';
+	} else if (computerChoice === 3) {
+		return 'lizard';
+	} else {
+		return 'spock';
+	}
 	return computerChoice;
 }
- 
-export default ComputerChoice;
+
+console.log(ComputerChoice());

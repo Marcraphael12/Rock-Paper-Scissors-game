027a5c66d3ac32dee0d6bdd66fafa2c4df94325d
Marc raphael <marcraphael979@gmail.com>
2022-07-18 20:16:02 +0100

    Create variable for buttons

---
+++ src/js/player.js
@@ -0,0 +1,7 @@
+const rock_btn = document.querySelector('.rock');
+const paper_btn = document.querySelector('.paper');
+const scissors_btn = document.querySelector('.scissor');
+const lizard_btn = document.querySelector('.lizard');
+const spock_btn = document.querySelector('.spock');
+
+const all_buttons = [rock_btn, paper_btn, scissors_btn, lizard_btn, spock_btn];
